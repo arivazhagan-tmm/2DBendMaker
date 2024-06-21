bd5b5fbd9499993a154ed15d0e0e86158754357a
Ajay Karthikeyan N <ajaykarthikeyan.n@metamation.com>
2024-06-21 16:33:06 +0530

    Refactored bend relief

--- BendAssists/BendRelief.cs
+++ BendAssists/BendRelief.cs
@@ -2,108 +2,135 @@
 
 #region class BendRelief --------------------------------------------------------------------------
 public class BendRelief {
-   #region Constructors ---------------------------------------------
-   public BendRelief () {
-      (mHasCornerNotch, mPLines) = (false, []);
-   }
-   #endregion
+    #region Constructors ---------------------------------------------
+    public BendRelief () => mPLines = [];
+    #endregion
 
-   #region Methods --------------------------------------------------
-   public BendProcessedPart ApplyBendRelief (Part profile) {
-      mPLines.Clear ();
-      foreach (var curve in profile.PLines) mPLines.Add (curve);
-      var hCurves = mPLines.Where (x => x.StartPoint.Y == x.EndPoint.Y).ToList (); // Horizontal curves
-      var vCurves = mPLines.Where (x => x.StartPoint.X == x.EndPoint.X).ToList (); // Vertical curves
-      var centroid = profile.Vertices.Centroid ();
-      PLine ICurve, NPCurve; // Intersected line, Nearest parallel line
-      BPoint NIPoint, IPoint; // Non intersecting point, Intersecting point
-      double offset; // Offset from nearest parallel line and bendline
-      if (profile.BendLines.Count () != 0) {
-         foreach (var bLine in profile.BendLines) {
-            ICurve = (bLine.Orientation == EBLOrientation.Horizontal ? vCurves : hCurves).Where (x => IsIntersecting (x, bLine)).First ();
-            NIPoint = (bLine.Orientation == EBLOrientation.Horizontal) ?
-                                  (bLine.StartPoint.X == ICurve.StartPoint.X ? bLine.EndPoint : bLine.StartPoint) :
-                                 (bLine.StartPoint.Y == ICurve.StartPoint.Y ? bLine.EndPoint : bLine.StartPoint);
-            mHasCornerNotch = (bLine.Orientation == EBLOrientation.Horizontal) ? vCurves.Where (x => x.StartPoint.X == NIPoint.X || x.EndPoint.X == NIPoint.X).Count () == 1
-                                                                                : hCurves.Where (x => x.StartPoint.Y == NIPoint.Y || x.EndPoint.Y == NIPoint.Y).Count () == 1;
-            IPoint = bLine.StartPoint == NIPoint ? bLine.EndPoint : bLine.StartPoint;
-            NPCurve = GetNearestParallelCurve ((bLine.Orientation == EBLOrientation.Horizontal) ? hCurves : vCurves, bLine);
-            offset = GetDistanceToLine (NPCurve, bLine);
-            AddBendReliefPoints (bLine, centroid, NIPoint, NPCurve, ICurve, offset, IPoint, profile.Thickness);
-         }
-      }
-      return new BendProcessedPart (EBDAlgorithm.EquallyDistributed, GetOrderedLines (mPLines), profile.BendLines, true);
-   }
+    #region Methods --------------------------------------------------
+    public BendProcessedPart ApplyBendRelief (Part profile) {
+        mPLines.Clear ();
+        foreach (var curve in profile.PLines) mPLines.Add (curve);
+        var hCurves = mPLines.Where (IsHorizontal).ToList ();
+        var vCurves = mPLines.Where (IsVertical).ToList ();
 
-   bool IsIntersecting (PLine curve, BendLine bLine) =>
-        bLine.Orientation == EBLOrientation.Horizontal ? (curve.StartPoint.X == bLine.StartPoint.X || curve.StartPoint.X == bLine.EndPoint.X) :
-                                                         (curve.StartPoint.Y == bLine.StartPoint.Y || curve.StartPoint.Y == bLine.EndPoint.Y);
+        if (profile.BendLines.Count () != 0)
+            foreach (var bLine in profile.BendLines) {
+                // Intersected Line - Line intersected by the bendline
+                var ILine = (IsHorizontal (bLine) ? vCurves : hCurves).Where (x => IsIntersecting (x, bLine)).First ();
 
-   PLine GetNearestParallelCurve (List<PLine> curves, BendLine bLine) => curves.OrderBy (curve => GetDistanceToLine (curve, bLine)).FirstOrDefault ();
+                // Nearest line from the bendline
+                var NLine = GetNearestParallelCurve (IsHorizontal (bLine) ? hCurves : vCurves, bLine);
 
-   double GetDistanceToLine (PLine curve, BendLine bLine) =>
-       bLine.Orientation == EBLOrientation.Horizontal
-       ? Math.Abs (curve.StartPoint.Y - bLine.StartPoint.Y)
-       : Math.Abs (curve.StartPoint.X - bLine.StartPoint.X);
+                // Non Intersecting Point of the bendline
+                var NIPoint = IsHorizontal (bLine) ? (EQ (bLine.StartPoint.X, ILine.StartPoint.X) ? bLine.EndPoint : bLine.StartPoint)
+                                                   : (EQ (bLine.StartPoint.Y, ILine.StartPoint.Y) ? bLine.EndPoint : bLine.StartPoint);
 
-   void AddBendReliefPoints (BendLine bLine, BPoint centre, BPoint NIPoint, PLine NPCurve, PLine ICurve, double offset, BPoint IPoint, double thickness) {
-      double brHeight = BendUtils.GetBendAllowance (90, 0.38, thickness, 2) / 2, brWidth = thickness / 2;
-      BPoint p1, p2, p3, p4, p5, p6;
-      bool isHorizontal = bLine.Orientation == EBLOrientation.Horizontal;
-      p1 = new (
-          NIPoint.X + (isHorizontal ? 0 : (NIPoint.X < centre.X ? -1 : 1) * offset),
-          NIPoint.Y + (isHorizontal ? ((NIPoint.Y > centre.Y ? 1 : -1) * offset) : 0)
-      );
-      p2 = new (
-          NIPoint.X + (isHorizontal ? 0 : (NIPoint.X < centre.X ? 1 : -1) * brHeight),
-          NIPoint.Y + (isHorizontal ? ((NIPoint.Y > centre.Y ? -1 : 1) * brHeight) : 0)
-      );
-      p3 = new (
-          p2.X + (isHorizontal ? ((ICurve.StartPoint.X > centre.X ? -1 : 1) * brWidth) : 0),
-          p2.Y + (isHorizontal ? 0 : ((ICurve.StartPoint.Y > centre.Y ? -1 : 1) * brWidth))
-      );
-      p4 = new (
-          (mHasCornerNotch ? NIPoint.X : p1.X) + (isHorizontal ? ((ICurve.StartPoint.X < centre.X ? 1 : -1) * brWidth) : 0),
-          (mHasCornerNotch ? NIPoint.Y : p1.Y) + (isHorizontal ? 0 : ((ICurve.StartPoint.Y < centre.Y ? 1 : -1) * brWidth))
-      );
-      p5 = new (
-          (mHasCornerNotch ? NIPoint.X : IPoint.X) + (isHorizontal ? 0 : (IPoint.X > centre.X ? 1 : -1) * offset),
-          (mHasCornerNotch ? NIPoint.Y : IPoint.Y) + (isHorizontal ? (IPoint.Y > centre.Y ? 1 : -1) * offset : 0)
-      );
-      p6 = (NPCurve.StartPoint.X, NPCurve.StartPoint.Y) == (p5.X, p5.Y)
-                            ? NPCurve.EndPoint : NPCurve.StartPoint;
+                // Intersecting point of bendline and the intersected line
+                var IPoint = EQ (bLine.StartPoint, NIPoint) ? bLine.EndPoint : bLine.StartPoint;
 
-      List<BPoint> points = bLine.Orientation is EBLOrientation.Horizontal ? ((IPoint.X > centre.X && IPoint.Y > centre.Y)
-                                                                  || (IPoint.X < centre.X && IPoint.Y < centre.Y))
-                                                                  ? [p5, p1, p2, p3, p4, p6] : [p6, p4, p3, p2, p1, p5]
-                                                                  : ((IPoint.X < centre.X && IPoint.Y > centre.Y)
-                                                                  || (IPoint.X > centre.X && IPoint.Y < centre.Y))
-                                                                  ? [p5, p1, p2, p3, p4, p6] : [p6, p4, p3, p2, p1, p5];
+                // Check if the profile has a stepcut
+                mHasStepCut = IsHorizontal (bLine) ? vCurves.Where (x => EQ (x.StartPoint.X, NIPoint.X) || EQ (x.EndPoint.X, NIPoint.X)).Count () == 1
+                                                      : hCurves.Where (x => EQ (x.StartPoint.Y, NIPoint.Y) || EQ (x.EndPoint.Y, NIPoint.Y)).Count () == 1;
+                AddBendReliefPoints (bLine, ILine, NLine, IPoint, NIPoint, profile.Vertices.Centroid (), profile.Thickness);
+            }
+        return new BendProcessedPart (EBDAlgorithm.EquallyDistributed, GetOrderedLines (mPLines), profile.BendLines, true);
+    }
 
-      if (mHasCornerNotch) points.Remove (p5);
-      for (int i = 0; i <= points.Count - 2; i++)
-         mPLines.Add (new PLine (ECurve.Line, -1, points[i], points[i + 1]));
-      mPLines.Remove (NPCurve);
-   }
+    // Get bend relief points and create 
+    void AddBendReliefPoints (BendLine bLine, PLine ILine, PLine NLine, BPoint IPoint, BPoint NIPoint, BPoint centre, double thickness) {
+        var isHorizontal = IsHorizontal (bLine);
 
-   public List<PLine> GetOrderedLines (List<PLine> pLines) {
-      List<PLine> orderedPLines = new () { pLines[0] };
-      pLines.RemoveAt (0);
-      while (pLines.Count > 0) {
-         PLine lastLine = orderedPLines.Last ();
-         BPoint lastEndPoint = lastLine.EndPoint;
-         var nxtLine = pLines.FirstOrDefault (line => line.StartPoint.X == lastEndPoint.X && line.StartPoint.Y == lastEndPoint.Y);
-         pLines.Remove (nxtLine);
-         orderedPLines.Add (nxtLine);
-      }
-      return orderedPLines;
-   }
+        // Offset between bendline and nearest parallel line
+        double offset = GetDistanceToLine (NLine, bLine),
+               brHeight = BendUtils.GetBendAllowance (90, 0.38, thickness, 2) / 2,
+               brWidth = thickness / 2;
 
-   #endregion
+        BPoint p1, p2, p3, p4, p5, p6;
+        p1 = new (
+            NIPoint.X + (isHorizontal ? 0 : (NIPoint.X < centre.X ? -1 : 1) * offset),
+            NIPoint.Y + (isHorizontal ? ((NIPoint.Y > centre.Y ? 1 : -1) * offset) : 0)
+        );
+        p2 = new (
+            NIPoint.X + (isHorizontal ? 0 : (NIPoint.X < centre.X ? 1 : -1) * brHeight),
+            NIPoint.Y + (isHorizontal ? ((NIPoint.Y > centre.Y ? -1 : 1) * brHeight) : 0)
+        );
+        p3 = new (
+            p2.X + (isHorizontal ? ((ILine.StartPoint.X > centre.X ? -1 : 1) * brWidth) : 0),
+            p2.Y + (isHorizontal ? 0 : ((ILine.StartPoint.Y > centre.Y ? -1 : 1) * brWidth))
+        );
+        p4 = new (
+            (mHasStepCut ? NIPoint.X : p1.X) + (isHorizontal ? ((ILine.StartPoint.X < centre.X ? 1 : -1) * brWidth) : 0),
+            (mHasStepCut ? NIPoint.Y : p1.Y) + (isHorizontal ? 0 : ((ILine.StartPoint.Y < centre.Y ? 1 : -1) * brWidth))
+        );
+        p5 = new (
+            (mHasStepCut ? NIPoint.X : IPoint.X) + (isHorizontal ? 0 : (IPoint.X > centre.X ? 1 : -1) * offset),
+            (mHasStepCut ? NIPoint.Y : IPoint.Y) + (isHorizontal ? (IPoint.Y > centre.Y ? 1 : -1) * offset : 0)
+        );
+        p6 = EQ (NLine.StartPoint, p5) ? NLine.EndPoint : NLine.StartPoint;
 
-   #region Private Data ---------------------------------------------
-   bool mHasCornerNotch;
-   List<PLine> mPLines;
-   #endregion
+        List<BPoint> orderedPoints = isHorizontal ? ((IPoint.X > centre.X && IPoint.Y > centre.Y)
+                                                 || (IPoint.X < centre.X && IPoint.Y < centre.Y))
+                                                  ? [p5, p1, p2, p3, p4, p6] : [p6, p4, p3, p2, p1, p5]
+                                                  : ((IPoint.X < centre.X && IPoint.Y > centre.Y)
+                                                 || (IPoint.X > centre.X && IPoint.Y < centre.Y))
+                                                  ? [p5, p1, p2, p3, p4, p6] : [p6, p4, p3, p2, p1, p5];
+
+        if (mHasStepCut) orderedPoints.Remove (p5);
+        for (int i = 0; i <= orderedPoints.Count - 2; i++)
+            mPLines.Add (new PLine (ECurve.Line, -1, orderedPoints[i], orderedPoints[i + 1]));
+        mPLines.Remove (NLine);
+    }
+
+    // Checks if line and bendline intersects
+    bool IsIntersecting (PLine line, BendLine bLine) =>
+         IsHorizontal (bLine) ? (EQ (line.StartPoint.X, bLine.StartPoint.X) || EQ (line.StartPoint.X, bLine.EndPoint.X))
+                              : (EQ (line.StartPoint.Y, bLine.StartPoint.Y) || EQ (line.StartPoint.Y, bLine.EndPoint.Y));
+
+    // Gets the nearest parallel curve to the bendline
+    PLine GetNearestParallelCurve (List<PLine> curves, BendLine bLine) =>
+        curves.OrderBy (curve => GetDistanceToLine (curve, bLine)).FirstOrDefault ();
+
+    // Get the distance of a line from the bendline
+    double GetDistanceToLine (PLine line, BendLine bLine) =>
+        IsHorizontal (bLine) ? Math.Abs (line.StartPoint.Y - bLine.StartPoint.Y)
+                             : Math.Abs (line.StartPoint.X - bLine.StartPoint.X);
+
+    public List<PLine> GetOrderedLines (List<PLine> pLines) {
+        List<PLine> orderedPLines = new () { pLines[0] };
+        pLines.RemoveAt (0);
+        while (pLines.Count > 0) {
+            BPoint LEPoint = orderedPLines.Last ().EndPoint; // Endpoint of last line
+            var nxtLine = pLines.FirstOrDefault (line => EQ (line.StartPoint, LEPoint));
+            orderedPLines.Add (nxtLine);
+            pLines.Remove (nxtLine);
+        }
+        return orderedPLines;
+    }
+    #endregion
+
+    #region Private Data --------------------------------------------
+    bool mHasStepCut;
+    List<PLine> mPLines;
+    #endregion
+
+    #region Commented ------------------------------------------------
+    //Methods to compare two doubles or two points
+    // Can be added inside Bend Utils class
+    bool EQ (double x, double y) => Math.Abs (x - y) < 1e-9;
+    bool EQ (BPoint p1, BPoint p2) {
+        double epsilon = 1e-9;
+        return Math.Abs (p1.X - p2.X) < epsilon && Math.Abs (p1.Y - p2.Y) < epsilon;
+    }
+
+    // Methods to check if a line or bendline is horizontal or vertical or inclined
+    // Can be added as properties to respective classes
+    // Used IsHorizontal method above
+    bool IsHorizontal (PLine line) => EQ (line.StartPoint.Y, line.EndPoint.Y);
+    bool IsVertical (PLine line) => EQ (line.StartPoint.X, line.EndPoint.X);
+    bool IsInclined (PLine line) => !EQ (line.StartPoint.Y, line.EndPoint.Y) && !EQ (line.StartPoint.X, line.EndPoint.X);
+    bool IsHorizontal (BendLine bLine) => bLine.Orientation == EBLOrientation.Horizontal;
+    bool IsVertical (BendLine bLine) => bLine.Orientation == EBLOrientation.Vertical;
+    bool IsInclined (BendLine bLine) => bLine.Orientation == EBLOrientation.Inclined;
+    #endregion
 }
 #endregion
